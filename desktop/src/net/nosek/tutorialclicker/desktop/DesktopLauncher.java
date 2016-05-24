4653ceacdd70615a6d86931bcc2d28f07445e83a
piternet <piternet.piotr@gmail.com>
2016-05-24 13:52:40 +0200

    Initial commit

---
+++ desktop/src/net/nosek/tutorialclicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.nosek.tutorialclicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.nosek.tutorialclicker.TutorialClickerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TutorialClickerGame(), config);
+	}
+}
