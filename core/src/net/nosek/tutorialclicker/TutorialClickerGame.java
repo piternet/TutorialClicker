b9753ff64e8906c9c8961a65e8ce33f508c4ce90
piternet <piternet.piotr@gmail.com>
2016-05-24 14:47:31 +0200

    config desktop launcher

--- core/src/net/nosek/tutorialclicker/TutorialClickerGame.java
+++ core/src/net/nosek/tutorialclicker/TutorialClickerGame.java
@@ -1,27 +1,34 @@
 package net.nosek.tutorialclicker;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import screens.SplashScreen;
 
-public class TutorialClickerGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class TutorialClickerGame extends Game {
+
+	public final static String GAME_NAME = "Tutorial Game";
+
+	public final static int WIDTH = 480;
+	public final static int HEIGHT = 700;
+
+	private boolean paused;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		this.setScreen(new SplashScreen(this));
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+//	Getters and setters
+
+	public boolean isPaused() {
+		return paused;
 	}
+
+	public void setPaused(boolean paused) {
+		this.paused = paused;
+	}
+
 }
