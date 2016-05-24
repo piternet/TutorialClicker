9a9aff69ed6ad9ce76cb33b87283b34e0047dbd6
piternet <piternet.piotr@gmail.com>
2016-05-24 22:48:52 +0200

    display Player image

--- core/src/screens/GameplayScreen.java
+++ core/src/screens/GameplayScreen.java
@@ -1,17 +1,40 @@
 package screens;
 
+import entities.Player;
 import net.nosek.tutorialclicker.TutorialClickerGame;
 
 /**
  * Created by piternet on 24.05.16.
  */
 public class GameplayScreen extends AbstractScreen {
+
+    private Player player;
+
     public GameplayScreen(TutorialClickerGame game) {
         super(game);
         init();
     }
 
     private void init() {
+        initPlayer();
+    }
+
+    private void initPlayer() {
+        player = new Player();
+        stage.addActor(player);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update();
+
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+    }
+
+    private void update() {
 
     }
 }
