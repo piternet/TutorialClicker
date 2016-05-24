287b4a003852fce134f2d8f6d4d90957351d65db
piternet <piternet.piotr@gmail.com>
2016-05-24 15:25:46 +0200

    added dummy GameplayScreen

---
+++ core/src/screens/GameplayScreen.java
@@ -0,0 +1,17 @@
+package screens;
+
+import net.nosek.tutorialclicker.TutorialClickerGame;
+
+/**
+ * Created by piternet on 24.05.16.
+ */
+public class GameplayScreen extends AbstractScreen {
+    public GameplayScreen(TutorialClickerGame game) {
+        super(game);
+        init();
+    }
+
+    private void init() {
+
+    }
+}
